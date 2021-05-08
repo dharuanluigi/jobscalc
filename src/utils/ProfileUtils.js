a0c53f18519001e0fc40d07dc61f59be02c73ddb
Dharuan Luigi <dharuanluigi@gmail.com>
2021-05-08 20:30:43 -0300

    Added: function to calc hour value

--- src/utils/ProfileUtils.js
+++ src/utils/ProfileUtils.js
@@ -3,10 +3,32 @@ module.exports = {
         return {
             name: profile_data.name,
             avatar: profile_data.avatar,
-            monthly_budget: profile_data['monthly-budget'],
-            hours_per_day: profile_data['hours-per-day'],
-            days_per_week: profile_data['days-per-week'],
-            vacation_per_year: profile_data['vacation-per-year'],
+            monthly_budget: Number(profile_data['monthly-budget']),
+            hours_per_day: Number(profile_data['hours-per-day']),
+            days_per_week: Number(profile_data['days-per-week']),
+            vacation_per_year: Number(profile_data['vacation-per-year']),
+            hour_value: profile_data.hour_value
+        }
+    },
+    calcHourValue(profile_data) {
+        // weeks per year
+        const weeks_per_year = 52
+
+        // remove vactions weeks from year to get how many weeks have in a month
+        const weeks_per_month = (weeks_per_year - Number(profile_data['vacation-per-year'])) / 12
+
+        // total hours woks in a week
+        const week_total_hours = Number(profile_data['hours-per-day']) * Number(profile_data['days-per-week'])
+
+        // hours worked in a week
+        const monthly_total_hours = week_total_hours * weeks_per_month
+
+        // value of a worked hour
+        const hour_value = Number(profile_data['monthly-budget']) / monthly_total_hours
+
+        return {
+            ...profile_data,
+            hour_value
         }
     }
 }
