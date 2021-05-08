ebbb33c0d442ed7f3e3ded1cc2d48c4f065a9d1c
Dharuan Luigi <dharuanluigi@gmail.com>
2021-05-08 20:30:18 -0300

    Feacture: update profile data db query

--- src/models/Profile.js
+++ src/models/Profile.js
@@ -1,4 +1,5 @@
 const Database = require('../db/configDB')
+const ProfileUtils = require('../utils/ProfileUtils')
 
 module.exports = {
     async get() {
@@ -13,7 +14,24 @@ module.exports = {
 
         return profile_data
     },
-    async update() {
-        
+    async update(profile_data) {
+
+        let profile_data_normalized = ProfileUtils.dataNormalizer(profile_data)
+
+        const connector = await Database()
+
+        await connector.run(`
+            UPDATE Profile SET
+            name = "${profile_data_normalized.name}",
+            avatar = "${profile_data_normalized.avatar}",
+            monthy_budget = ${profile_data_normalized.monthly_budget},
+            hours_per_day = ${profile_data_normalized.hours_per_day},
+            days_per_week = ${profile_data_normalized.days_per_week},
+            vacation_per_year = ${profile_data_normalized.vacation_per_year},
+            hour_value = ${profile_data_normalized.hour_value}
+            WHERE id = 1;
+        `)
+
+        await connector.close()
     },
 }
