18eb1869cd43d006f9dce3cb9bbaf84e56db8ca5
Dharuan Luigi <dharuanluigi@gmail.com>
2021-05-09 20:50:36 -0300

    Added: status panel information

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -1,12 +1,17 @@
+const DashboardUtils = require('../utils/DashboardUtils')
 const ProfileController = require('../controllers/ProfileController')
 const JobController = require('../controllers/JobController')
 
 module.exports = {
     async index(req, res) {
+        // get profile data
         const profile_data = await ProfileController.getProfileData()
 
+        // get all jobs from DB
         const jobs = await JobController.getAllJobs()
 
-        return res.render('index', { profile: profile_data, jobs })
+        const status_panel = DashboardUtils.calcStatusPanel(jobs, profile_data)
+
+        return res.render('index', { profile: profile_data, jobs, status_panel })
     },
 }
