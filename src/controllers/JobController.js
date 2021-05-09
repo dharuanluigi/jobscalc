dde513e9c3a47fb86910f6398bda3427850a7ea5
Dharuan Luigi <dharuanluigi@gmail.com>
2021-05-08 22:01:19 -0300

    Added: add function to add new job into DB

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -1,3 +1,6 @@
+const JobUtils = require('../utils/JobUtils')
+const Job = require('../models/Job')
+
 module.exports = {
     addJobPage(req, res) {
         return res.render('job')
@@ -5,4 +8,10 @@ module.exports = {
     editJobPage(req, res) {
         return res.render('job-edit')
     },
+    async addNewJob(req, res) {
+
+        await Job.add(JobUtils.dataNormalizer(JobUtils.calcDueDate(await JobUtils.calcBudget(req.body))))
+        
+        return res.redirect('/')
+    }
 }
