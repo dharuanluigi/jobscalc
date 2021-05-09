9d60ed8a981b50a8ad00070ef75be912f3002154
Dharuan Luigi <dharuanluigi@gmail.com>
2021-05-09 17:29:13 -0300

    Update: file resp for data and func calc proj time

--- src/utils/JobUtils.js
+++ src/utils/JobUtils.js
@@ -1,4 +1,4 @@
-const Profile = require('../models/Profile')
+const ProfileController = require('../controllers/ProfileController')
 
 module.exports = {
     dataNormalizer(job_data) {
@@ -6,19 +6,42 @@ module.exports = {
             name: job_data.name,
             daily_hours: Number(job_data['daily-hours']),
             total_hours: Number(job_data['total-hours']),
+            created_at: Number(job_data.created_at),
             dueDate: Number(job_data.dueDate),
             budget: Number(job_data.budget)
         }
     },
     calcDueDate(job_data) {
+        // how many days that work will finished: 5::
+        const total_days = Math.ceil(Number(job_data['total-hours']) / Number(job_data['daily-hours']))
+        
+        // just for plus days
+        const created_date = new Date(job_data.created_at)
+
+        // get day to create and plus with total days to delivery
+        const due_day = created_date.getDate() + total_days
+
+        
+        // calc due day from create date
+        const due_date_ms = created_date.setDate(due_day)
+        
+        // diff btw right now and due date
+        const diff_dates = Math.ceil(due_date_ms - Date.now())
+        
+        // a day in ms: 86400000
+        const day_ms = 1000*60*60*24
+
+        // discovering how many days, transform diff days ms in commum days
+        const delivery_day = Math.ceil(diff_dates / day_ms)
+
         return {
             ...job_data,
-            dueDate: 2
+            dueDate: delivery_day
         }
     },
     async calcBudget(job_data) {
         // get value hour and * for total hours
-        const profile_data = await Profile.get()
+        const profile_data = await ProfileController.getProfileData()
         return {
             ...job_data,
             budget: Number(job_data['total-hours']) * profile_data.hour_value
