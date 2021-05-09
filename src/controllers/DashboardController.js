10619b7cd3651f6e4cc965894acd4b075543d890
Dharuan Luigi <dharuanluigi@gmail.com>
2021-05-09 15:59:36 -0300

    Updated: set file responsability to control data

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -1,10 +1,12 @@
-const Profile = require('../models/Profile')
+const ProfileController = require('../controllers/ProfileController')
+const JobController = require('../controllers/JobController')
 
 module.exports = {
     async index(req, res) {
+        const profile_data = await ProfileController.getProfileData()
 
-        const profile_data = await Profile.get()
+        const jobs = await JobController.getAllJobs()
 
-        return res.render('index', { profile: profile_data })
+        return res.render('index', { profile: profile_data, jobs })
     },
 }
