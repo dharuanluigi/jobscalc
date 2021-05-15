190d66c9e636fa18e1183af31661d76afe8e91d4
Dharuan Luigi <dharuanluigi@gmail.com>
2021-05-15 17:08:59 -0300

    Change: show just once alert when empty value fnd

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -3,10 +3,16 @@ btn_save.addEventListener('click', checkFields)
 
 function checkFields() {
     const fields = document.getElementsByTagName('input')
+    let empty_value = false
+
     for(let field of fields) {
         if(isEmpty(field)) {
-            window.alert('Todos os campos devem ser preenchidos!')
+            empty_value = true        
         }
+    }
+
+    if(empty_value) {
+        window.alert('Todos os campos devem ser preenchidos!')
     }
 }
 
