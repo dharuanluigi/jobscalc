854374c960bdae42d868cb7e31f16e320733dcaa
Dharuan Luigi <dharuanluigi@gmail.com>
2021-05-15 17:11:43 -0300

    added file to responsability general funcs

---
+++ src/utils/Utils.js
@@ -0,0 +1,14 @@
+module.exports = {
+    checkFields(fields) {
+        let isEmpty = false
+
+        // verify if a field has an empty value
+        Object.keys(fields).forEach((field) => {
+            if(!fields[field]) {
+                isEmpty = true 
+            }
+        })
+
+        return isEmpty
+    }
+}
