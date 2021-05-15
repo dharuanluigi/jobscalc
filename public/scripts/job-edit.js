56b2ed650c1e9e70f510872ca56a07df12a168ab
Dharuan Luigi <dharuanluigi@gmail.com>
2021-05-15 17:27:25 -0300

    Implements func to check fields

--- public/scripts/job-edit.js
+++ public/scripts/job-edit.js
@@ -5,3 +5,26 @@ const modal = Modal({ animateClasses: ['animate-pop', 'back'] })
 document
   .querySelector('.open-modal')
   .addEventListener('click', modal.open)
+
+// add field validation
+const btn_save = document.getElementById('btnSave')
+btn_save.addEventListener('click', checkFields)
+
+function checkFields() {
+  const fields = document.getElementsByTagName('input')
+  let empty_value = false
+
+  for(let field of fields) {
+      if(isEmpty(field)) {
+          empty_value = true
+      }
+  }
+
+  if(empty_value) {
+      window.alert('Todos os campos devem ser preenchidos!')
+  }
+}
+
+function isEmpty(field) {
+  return field.value == ''
+}
