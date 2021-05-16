43131dce4dbaa1d33be3e2f2f8b87338cd439839
Dharuan Luigi <dharuanluigi@gmail.com>
2021-05-16 17:14:53 -0300

    added get data wiich usr

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -3,15 +3,44 @@ const ProfileController = require('../controllers/ProfileController')
 const JobController = require('../controllers/JobController')
 
 module.exports = {
-    async index(req, res) {
+    async getLogedProfileData(user_id) {
         // get profile data
-        const profile_data = await ProfileController.getProfileData()
+        const profile_data = await ProfileController.getProfileData(user_id)
 
         // get all jobs from DB
         const jobs = await JobController.getAllJobs()
 
         const status_panel = DashboardUtils.calcStatusPanel(jobs, profile_data)
 
-        return res.render('index', { profile: profile_data, jobs, status_panel })
+        return {
+            profile_data,
+            jobs,
+            status_panel
+        }
     },
+    async index(req, res) {
+        const user_data_login = await ProfileController.login(req.body)
+        if(user_data_login) {
+            // save login in a session
+            req.session.data = {
+                user_id: user_data_login.id
+            }
+
+            const data = await module.exports.getLogedProfileData(req.session.data.user_id)
+           
+           return res.render('index', { profile: data.profile_data, jobs: data.jobs, status_panel: data.status_panel })
+        }
+        else {
+            return res.send('Credênciais inválidas!')
+        }
+    },
+    async loginPage(req, res) {
+        if(req.session.data) {
+            const data = await module.exports.getLogedProfileData(req.session.data.user_id)
+            return res.render('index', { profile: data.profile_data, jobs: data.jobs, status_panel: data.status_panel })
+        }
+        else {
+           return res.render('login')
+        }
+    }
 }
