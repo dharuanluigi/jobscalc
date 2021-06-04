33dee03b5f4424bebd540bfcb78da93ab739dfe5
Dharuan Luigi <dharuanluigi@gmail.com>
2021-06-04 20:51:13 -0300

    Name length limitator

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -3,19 +3,30 @@ btn_save.addEventListener('click', checkFields)
 
 function checkFields() {
     const fields = document.getElementsByTagName('input')
+    
+    // verify if fields are empty
     let empty_value = false
-
     for(let field of fields) {
         if(isEmpty(field)) {
             empty_value = true        
         }
     }
-
     if(empty_value) {
         window.alert('Todos os campos devem ser preenchidos!')
     }
+
+    const inputName = document.getElementById('name')
+    if(nameLengthVer(inputName)) {
+        window.alert('O nome não pode ter mais que 30 caracteres!')
+    }
+    
 }
 
 function isEmpty(field) {
     return field.value == '' || field.value.trim().length == 0
+}
+
+function nameLengthVer(field) {
+    const content = field.value
+    return content.trim().length > 30
 }
